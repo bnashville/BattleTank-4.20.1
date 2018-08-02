754dbcff1730bf4ca99f2384437466820ad1a6ad
DESKTOP-GTB8VQ5\Brian <bcofer@gmail.com>
2018-08-02 18:28:57 -0500

    165: 4:49

--- Source/BattleTank/Private/TankMovementComponent.cpp
+++ Source/BattleTank/Private/TankMovementComponent.cpp
@@ -2,11 +2,17 @@
 
 #include "TankMovementComponent.h"
 
+void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+
+	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
 	UE_LOG(LogTemp, Warning, TEXT("Intend move forward: %f"), Throw)
-
-
 }
 
